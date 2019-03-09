93c2c23a680614021ae4dd347dfea4ff3129b1c8
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-03-09 12:53:35 +0530

    fetch data from firebase

--- src/App.js
+++ src/App.js
@@ -1,78 +1,126 @@
 import React, { Component } from "react";
 import { fire } from "./config/fbConfig";
 import "./App.css";
-
+import image from "./static/images/profile.png";
+import gif from "./static/images/ring2.gif";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subscriber: {
-        username: "",
-        phone_number: ""
-      }
+      subscribers: [],
+      loading: false
     };
 
     this.itemsRef = fire.ref("subscriber");
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
     this.itemsRef.on("value", data => {
-      var subscriber = data.val();
+      var subscriber = data.val() || {};
+      console.log(subscriber);
       var keys = Object.keys(subscriber);
-      console.log(keys);
-      for (var i = 0; i < keys.length; i++) {
-        var k = keys[i];
-        console.log(subscriber[k].username);
-      }
+      const subscribers = keys.map(key => ({ key, ...subscriber[key] }));
+      this.setState({ subscribers, loading: false });
     });
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    this.itemsRef.push({
-      username: this.refs.username.value,
-      phone_number: this.refs.phone_number.value
-    });
+    if (this.refs.phone_number.value.length !== 10) {
+      alert("Invalid Phone Number");
+    } else {
+      this.itemsRef.push({
+        username: this.refs.username.value,
+        phone_number: this.refs.phone_number.value
+      });
+    }
   };
 
-  handleDelete = id => {
+  deleteSubscriber = id => {
     this.itemsRef.update({
-      [id]: null
+      [id.key]: null
     });
   };
 
   render() {
+    const { length: count } = this.state.subscribers;
     return (
-      <div className="container">
-        <h1> Phone Directory </h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group" id="spc-1">
-            <div className="input-1">
-              <i className="fa fa-user" />
-              <input
-                type="text"
-                className="form-control"
-                ref="username"
-                placeholder="Enter name"
-              />
-            </div>
+      <React.Fragment>
+        <div className="container">
+          <div>
+            <img className="logo" src={image} alt="logo" />
+            <h1> Phone Directory </h1>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group" id="spc-1">
+                <div className="input-1">
+                  <i className="fa fa-user" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    ref="username"
+                    placeholder="Enter name"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="input-1">
+                  <i className="fa fa-phone" />
+                  <input
+                    type="number"
+                    className="form-control"
+                    minLength="10"
+                    maxLength="10"
+                    size="10"
+                    ref="phone_number"
+                    placeholder="Enter phone number"
+                    required
+                  />
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Subscribe
+              </button>
+            </form>
           </div>
-          <div className="form-group">
-            <div className="input-1">
-              <i className="fa fa-phone" />
-              <input
-                type="tel"
-                className="form-control"
-                ref="phone_number"
-                placeholder="Enter phone number"
-              />
-            </div>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit{" "}
-          </button>
-        </form>
-        <div>
-          <p className="para" ref="para" />
         </div>
-      </div>
+        {this.state.loading ? (
+          <div className="gif">
+            <img src={gif} alt="gif" />
+          </div>
+        ) : count === 0 ? (
+          <p className="gif">There are no subscriber in Database</p>
+        ) : (
+          <div className="list">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Mobile Number</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.subscribers.map(subscriber => (
+                  <tr key={subscriber.key}>
+                    <td>{subscriber.username}</td>
+                    <td>{subscriber.phone_number}</td>
+                    <td>
+                      <button
+                        className="unsubscribe--button"
+                        onClick={() => this.deleteSubscriber(subscriber)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </React.Fragment>
     );
   }
 }
