67a44ebbee0fe07c9fd72bb8e216af0f08c06d6b
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-03-17 12:46:01 +0530

    Updates

---
+++ precache-manifest.1cb7d3ed167db383056fd4475d4a590d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb31e98620303e47f5df",
+    "url": "/phoneDirectory/static/css/main.537a64d0.chunk.css"
+  },
+  {
+    "revision": "fb31e98620303e47f5df",
+    "url": "/phoneDirectory/static/js/main.c3d0a9c7.chunk.js"
+  },
+  {
+    "revision": "fb08e304f95f5f01dc82",
+    "url": "/phoneDirectory/static/js/runtime~main.6ef97947.js"
+  },
+  {
+    "revision": "a5a3904c774f194989f3",
+    "url": "/phoneDirectory/static/js/2.049bbbb2.chunk.js"
+  },
+  {
+    "revision": "a1d40c3876bd1460ff1e9e3858d699c7",
+    "url": "/phoneDirectory/static/media/profile.a1d40c38.png"
+  },
+  {
+    "revision": "a697fcc4ed0577334cda3f9df06f43e5",
+    "url": "/phoneDirectory/static/media/ring2.a697fcc4.gif"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/phoneDirectory/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/phoneDirectory/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/phoneDirectory/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "43026c24c90241933798258c5aa8f80b",
+    "url": "/phoneDirectory/index.html"
+  }
+];
